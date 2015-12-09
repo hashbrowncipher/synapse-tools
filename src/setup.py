2fbe57b562d33fe9c493e6413d43b1016178b748
Evan Krall <krall@yelp.com>
2015-12-09 11:47:07 -0800

    bump python package version to match deb version

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='synapse-tools',
-    version='0.8.15',
+    version='0.9.0',
     provides=['synapse_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
